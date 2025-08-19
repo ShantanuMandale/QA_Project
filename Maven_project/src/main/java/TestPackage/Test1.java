d66c0c3cbfa9d84ad287dc2f36d1c210cfc344c0
ShantanuMandale <140900064+ShantanuMandale@users.noreply.github.com>
2025-08-19 14:12:49 +0530

    Add project

---
+++ Maven_project/src/main/java/TestPackage/Test1.java
@@ -0,0 +1,10 @@
+package TestPackage;
+
+import org.testng.annotations.Test;   
+public class Test1 {
+
+    @Test
+    public void a() {
+        System.out.println("Hello a test method");
+    }
+}
