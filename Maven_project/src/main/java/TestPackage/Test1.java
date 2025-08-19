63be6fd3ca5a5056a9af7764577dcd7e9ba8dfaa
ShantanuMandale <140900064+ShantanuMandale@users.noreply.github.com>
2025-08-19 20:21:46 +0530

    updated

--- Maven_project/src/main/java/TestPackage/Test1.java
+++ Maven_project/src/main/java/TestPackage/Test1.java
@@ -1,10 +1,20 @@
 package TestPackage;
 
-import org.testng.annotations.Test;   
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;   
 public class Test1 {
 
     @Test
     public void a() {
-        System.out.println("Hello a test method");
+    	System.out.println("Hello a test method");
+    	WebDriverManager.chromedriver().setup();
+    	WebDriver driver = new ChromeDriver();
+    	
+    	driver.get("https://github.com/ShantanuMandale/QA_Project");
+    	System.out.println("Url opened");
+        
     }
 }
